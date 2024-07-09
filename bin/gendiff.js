deb88a1fb716371bc689689718455caaf1b4bd47
lev krasnov <leevkrasnov@gmail.com>
2024-07-09 13:22:23 +0500

    Add Options. Step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,5 +3,9 @@ import { program } from 'commander';
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference.');
+
+program
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
